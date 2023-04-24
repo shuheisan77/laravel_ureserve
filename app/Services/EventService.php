45d6b6a8b2eb1e5b1e2952ab6459b5612e1d50e9
shuheisan77 <shuheisan0420@icloud.com>
2023-04-24 22:40:30 +0900

    event store

---
+++ app/Services/EventService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class EventService
+{
+    public static function checkEventDuplication($eventDate, $startTime, $endTime)
+    {
+    $check = DB::table('events')
+        ->whereDate('start_date', $eventDate)
+        ->whereTime('end_date', '>', $startTime)
+        ->whereTime('start_date', '<', $endTime)
+        ->exists();
+    return $check;
+    }
+
+    public static function joinDateAndTime($date, $time)
+    {
+        $join = $date . " " . $time;
+        return Carbon::createFromFormat('Y-m-d H:i', $join);
+    }
+}
