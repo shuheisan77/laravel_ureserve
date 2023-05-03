091371ee7ad9f9f06a7e5f6a4dba741f4071af98
shuheisan77 <shuheisan0420@icloud.com>
2023-05-03 12:26:42 +0900

    event dummy 修正

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -9,21 +9,26 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class EventFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition()
     {
+        $availableHour = $this->faker->numberBetween(10, 18);
+        $minutes = [0, 30];
+        $mKey = array_rand($minutes);
+        $addHour = $this->faker->numberBetween(1, 3);
+
         $dummyDate = $this->faker->dateTimeThisMonth();
+        $startDate = $dummyDate->setTime($availableHour, $minutes[$mKey]);
+        $clone = clone $startDate;
+        $endDate = $clone->modify('+'.$addHour.'hour');
+        // dd($startDate, $endDate);
+
 
         return [
             'name' => $this->faker->name,
             'information' => $this->faker->realText,
             'max_people' => $this->faker->numberBetween(1,20),
-            'start_date' => $dummyDate->format('Y-m-d H:i:s'),
-            'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'),
+            'start_date' => $startDate,
+            'end_date' => $endDate,
             'is_visible' => $this->faker->boolean()
         ];
     }
