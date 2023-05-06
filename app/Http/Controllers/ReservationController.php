ac5b877ef8b22afa19efbcbbf2d92e7a1f71aa41
shuheisan77 <shuheisan0420@icloud.com>
2023-05-06 16:01:52 +0900

    mypage

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -33,7 +33,14 @@ class ReservationController extends Controller
         else{
             $reservablePeople = $event->max_people;
         }
-        return view('event-detail', compact('event', 'reservablePeople'));
+
+        $isReserved = Reservation::where('user_id', '=', Auth::id())
+        ->where('event_id', '=', $id)
+        ->where('canceled_date', '=', null)
+        ->latest()
+        ->first();
+
+        return view('event-detail', compact('event', 'reservablePeople', 'isReserved'));
         
     }
 
